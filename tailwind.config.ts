57cd0fc8ca875b6b7f4e82f92e1c8b5f134bbcd6
Jemimah Mae Isaig <jcisaig@up.edu.ph>
2024-07-11 13:38:18 +0800

    feat: add dependencies, font, theme

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss';
+import themeAUB from './themeAUB.ts';
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
+/** @type {import('tailwindcss').Config} \*/
+export default {
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+        join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}')
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        skeleton({
+            themes: [ 
+                themes.cerberus, 
+                themes.rose,
+                // Custom Theme
+                themeAUB
+             ]
+            
+        })
+    ]
+}
