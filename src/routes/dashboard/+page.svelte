79cee2a83701144f15d4b9bfe642df0e031077c4
omeeeeeeee <naamparo@up.edu.ph>
2024-07-12 11:06:17 +0800

    working skeleton appbar component in dashboard route

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar title="Hi" />
+
+<style>
+  main {
+    padding: 1em;
+    max-width: 800px;
+    margin: 0 auto;
+  }
+</style>
+
+<main>
+  <AppBar title="Hi" />
+  <p>Welcome to the dashboard hehe</p>
+</main>
