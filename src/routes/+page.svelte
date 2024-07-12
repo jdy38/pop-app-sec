05bb47e9b485d5376566cc238519cef2257aaa31
Jemimah Mae Isaig <jcisaig@up.edu.ph>
2024-07-12 11:17:15 +0800

    feat: add dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import bg from '$lib/assets/bg.webp';
+    let bgStyle = `background-image: url(${bg});`;
+</script>
+
+
+<style>
+    .bg-container {
+        position: relative;
+        width: 100vw;
+        height: 100vh;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+
+
+    .bg-container::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(219, 215, 215, 0.5);
+    }
+</style>
+
+
+<div class="bg-container" style={bgStyle}>
+</div>
