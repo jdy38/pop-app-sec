82aaabd6d2ac24279774a8a23d66299278d60969
omeeeeeeee <naamparo@up.edu.ph>
2024-07-12 14:35:30 +0800

    added header

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { AppBar } from '@skeletonlabs/skeleton';
+    import logo from '$lib/assets/logo.webp';
+</script>
+
+<div class="div-1">
+    <AppBar>
+        <svelte:fragment slot="lead">
+            <div class="logo-container">
+                <img src= {logo} alt="Logo" style="height: 70px; width: auto;" />
+            </div>
+        </svelte:fragment>
+        <svelte:fragment slot="trail">
+            <p class="font-sans text-white">Hi, Jed!</p>
+
+            <div class="circle-placeholder"></div>
+
+            <p class="font-sans text-yellow-500 underline">Logout</p>
+        </svelte:fragment>
+    </AppBar>
+</div>
+
+<style>
+    .div-1 {
+        background-color: #C33521;
+    }
+
+    .circle-placeholder {
+        width: 40px; 
+        height: 40px;
+        border-radius: 50%; 
+        background-color: #ccc; 
+        margin-right: 10px; 
+        display: inline-block; 
+    }
+</style>
+
