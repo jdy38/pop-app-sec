5abb9b0d61b576bb0cc032d979313d464102b577
mavricklee <mavrick.lee@obf.ateneo.edu>
2024-07-19 18:03:08 +0800

    Installed necessary packages for backend. Setup database.

---
+++ src/routes/database_test/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<svelte:head>
+	<title>All Chapters</title>
+</svelte:head>
+
+<section>
+	<h1>All Chapters</h1>
+
+	{#if data.chapters && data.chapters.length}
+		<table>
+			<thead>
+				<tr>
+					<th scope="col">Chapter ID</th>
+					<th scope="col">Chapter Name</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				{#each data.chapters as chapter (chapter.chapterID)}
+					<tr>
+						<td>
+							<a href="/books/{chapter.chapterID}">{chapter.chapterID}</a>
+						</td>
+						<td>{chapter.chapterName}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{/if}
+</section>
